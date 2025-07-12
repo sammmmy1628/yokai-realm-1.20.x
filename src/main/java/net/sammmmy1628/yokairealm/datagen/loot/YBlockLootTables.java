46cdf96a42ca70b283dddbb429ca00d145ff3a73
weng1ii <shandabylsasha@gmail.com>
2025-07-13 02:01:58 +0300

    + fixed crops drop rates

--- src/main/java/net/sammmmy1628/yokairealm/datagen/loot/YBlockLootTables.java
+++ src/main/java/net/sammmmy1628/yokairealm/datagen/loot/YBlockLootTables.java
@@ -3,9 +3,17 @@ package net.sammmmy1628.yokairealm.datagen.loot;
 import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import net.minecraftforge.registries.RegistryObject;
 import net.sammmmy1628.yokairealm.block.YokaiBlocks;
 import net.sammmmy1628.yokairealm.block.custom.CucumberCropBlock;
@@ -23,18 +31,37 @@ public class YBlockLootTables extends BlockLootSubProvider {
     protected void generate() {
         LootItemCondition.Builder lootitemcondition$builder = LootItemBlockStatePropertyCondition
                 .hasBlockStateProperties(YokaiBlocks.EGGPLANT_CROP.get())
-                .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(EggplantCropBlock.AGE, 5));
+                .setProperties(StatePropertiesPredicate.Builder.properties()
+                        .hasProperty(EggplantCropBlock.AGE, 5)
+                        .hasProperty(EggplantCropBlock.HALF, DoubleBlockHalf.LOWER));
 
         this.add(YokaiBlocks.EGGPLANT_CROP.get(), createCropDrops(YokaiBlocks.EGGPLANT_CROP.get(), YokaiItems.EGGPLANT.get(),
                 YokaiItems.EGGPLANT_SEED.get(), lootitemcondition$builder));
 
         LootItemCondition.Builder lootitemcondition$builder2 = LootItemBlockStatePropertyCondition
                 .hasBlockStateProperties(YokaiBlocks.CUCUMBER_CROP.get())
-                .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(CucumberCropBlock.AGE, 5));
+                .setProperties(StatePropertiesPredicate.Builder.properties()
+                        .hasProperty(CucumberCropBlock.AGE, 5)
+                        .hasProperty(EggplantCropBlock.HALF, DoubleBlockHalf.LOWER));
 
         this.add(YokaiBlocks.CUCUMBER_CROP.get(), createCropDrops(YokaiBlocks.CUCUMBER_CROP.get(), YokaiItems.CUCUMBER.get(),
                 YokaiItems.CUCUMBER.get(), lootitemcondition$builder2));//cucumber_seed
     }
+
+    @Override
+    protected LootTable.Builder createCropDrops(Block crop, Item drop, Item seeds, LootItemCondition.Builder condition) {
+        return LootTable.lootTable()
+                .withPool(LootPool.lootPool()
+                        .add(LootItem.lootTableItem(drop)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 2.0F))))
+                        .when(condition))
+                .withPool(LootPool.lootPool()
+                        .add(LootItem.lootTableItem(seeds)
+                                .apply(SetItemCountFunction.setCount(ConstantValue.exactly(2))))
+                        .when(condition)
+                );
+    }
+
 
     @Override
     protected Iterable<Block> getKnownBlocks() {
